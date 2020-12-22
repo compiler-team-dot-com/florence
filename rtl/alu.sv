494c2b901142a124d8358478a181a8016a1d0047
Bruno Flores <bruno@brunoflores.com.br>
2020-12-22 22:04:11 +1000

    Update ALU.

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,12 +1,12 @@
 module alu #(
     parameter SIZE = 7
+) (
+    input logic [1:0] operator_i,
+    input logic [SIZE:0] operand_a_i,
+    input logic [SIZE:0] operand_b_i,
+
+    output logic [SIZE:0] result_o
 );
-
-  input logic [1:0] operator_i;
-  input logic [SIZE:0] operand_a_i;
-  input logic [SIZE:0] operand_b_i;
-
-  output logic [SIZE:0] result_o;
 
   always_comb begin
     case (operator_i)
--- rtl/alu_tb.sv
+++ rtl/alu_tb.sv
@@ -1,10 +1,21 @@
-`timescale 1ns / 1ps
+`timescale 1ns / 1ns
 
 module alu_tb;
-  integer SIZE 7;
+  reg [7:0] a, b;
+  reg [1:0] sel;
+  reg [7:0] out;
 
-  reg[SIZE:0] a, b;
-  reg[1:0] sel;
+  initial begin
+    sel = 0;
+    a   = 3;
+    b   = 2;
+    #5 sel = 1;
+  end
 
-  
+  alu dut (
+      .operator_i (sel),
+      .operand_a_i(a),
+      .operand_b_i(b),
+      .result_o   (out)
+  );
 endmodule
